a8f5d51d9756bab974d55a49f79c7b11c52a262c
Roman <tsvetkovv22@gmail.com>
2019-07-18 16:13:52 +1000

    Improved config

--- webpack.config.js
+++ webpack.config.js
@@ -1,100 +1,112 @@
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WebpackMessages = require('webpack-messages');
+
 const path = require('path');
 
-module.exports = {
-  entry: ['babel-polyfill', './src/index.jsx'],
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: '[name].js',
-    publicPath: '/',
-  },
-  stats: 'minimal',
-  devServer: {
-    historyApiFallback: true,
-    open: true,
-    hot: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: '/.html$/',
-        use: [
-          {
-            loader: 'html-loader',
-          },
-        ],
-      },
-      {
-        test: /.(png|jpe?g)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              mimetype: 'image/png',
-            },
-          },
-        ],
-      },
-      {
-        test: /.svg$/,
-        use: 'file-loader',
-      },
-      {
-        test: /\.(scss|css)$/,
-        exclude: '/node_modules/',
-        use: [
-          {
-            loader: 'style-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: './public/index.html',
-    }),
-    new WebpackMessages({
-      name: 'client',
-      // eslint-disable-next-line
-      logger: str => console.log(`>> ${str}`),
-    }),
-  ],
-  resolve: {
-    modules: [path.resolve(__dirname, './src'), 'node_modules'],
-    extensions: ['.js', '.jsx'],
-    alias: {
-      'react-dom': '@hot-loader/react-dom',
-      components: path.resolve(__dirname, './src/components'),
-      constants: path.resolve(__dirname, './src/constants'),
-      containers: path.resolve(__dirname, './src/containers'),
-      pages: path.resolve(__dirname, './src/pages'),
-      store: path.resolve(__dirname, './src/store'),
-      static: path.resolve(__dirname, './src/static'),
-      api: path.resolve(__dirname, './src/api'),
+module.exports = (_, argv) => {
+  const isDevelopment = argv.mode === 'development';
+
+  return {
+    entry: ['babel-polyfill', './src/index.jsx'],
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: '[name].js',
+      publicPath: '/',
     },
-  },
+    stats: 'minimal',
+    devServer: {
+      historyApiFallback: true,
+      open: true,
+      hot: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: '/.html$/',
+          use: [
+            {
+              loader: 'html-loader',
+            },
+          ],
+        },
+        {
+          test: /.(png|jpe?g)$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                mimetype: 'image/png',
+              },
+            },
+          ],
+        },
+        {
+          test: /.svg$/,
+          use: 'file-loader',
+        },
+        {
+          test: /\.(scss|css)$/,
+          exclude: '/node_modules/',
+          loader: [
+            isDevelopment
+              ? {
+                loader: 'style-loader',
+                options: {
+                  sourceMap: true,
+                },
+              }
+              : MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: isDevelopment,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDevelopment,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HTMLWebpackPlugin({
+        template: './public/index.html',
+      }),
+      new WebpackMessages({
+        name: 'client',
+        // eslint-disable-next-line
+        logger: str => console.log(`>> ${str}`),
+      }),
+      new MiniCssExtractPlugin({
+        filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+        chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css',
+      }),
+    ],
+    resolve: {
+      modules: [path.resolve(__dirname, './src'), 'node_modules'],
+      extensions: ['.js', '.jsx'],
+      alias: {
+        'react-dom': '@hot-loader/react-dom',
+        components: path.resolve(__dirname, './src/components'),
+        constants: path.resolve(__dirname, './src/constants'),
+        containers: path.resolve(__dirname, './src/containers'),
+        pages: path.resolve(__dirname, './src/pages'),
+        store: path.resolve(__dirname, './src/store'),
+        static: path.resolve(__dirname, './src/static'),
+        api: path.resolve(__dirname, './src/api'),
+      },
+    },
+  };
 };
