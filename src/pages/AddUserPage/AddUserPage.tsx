0df1f45a7958946efac135ed54cdc3ffbb832bc4
Roman Tsvetkov <tsvetkovv22@gmail.com>
2021-04-22 09:43:01 +0300

    chore: removed tests

--- src/pages/AddUserPage/AddUserPage.tsx
+++ src/pages/AddUserPage/AddUserPage.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
 
 import { AddUserFormContainer as AddUserPageForm } from 'containers';
 
 import './AddUserPage.scss';
 
-export const AddUserPage = ({ history, hasError, errors, isSending }) => (
+type Props = {
+  hasError: boolean;
+  errors: Array<{ message: string }>;
+  isSending: boolean;
+} & RouteComponentProps;
+
+export const AddUserPage: React.FC<Props> = ({ history }) => (
   <div className="add-user-page">
     <div className="container">
       <h1 className="add-user-page__title mb40">Add a user:</h1>
       <div className="add-user-page__content box">
-        <AddUserPageForm
-          goBack={history.goBack}
-          hasError={hasError}
-          errors={errors}
-          isSending={isSending}
-        />
+        <AddUserPageForm goBack={history.goBack} />
       </div>
     </div>
   </div>
